28e832d451bf4c59d5dd50752d36fc81a2329764
Michael Sanabria <71748191+michaeldevelopment@users.noreply.github.com>
2022-01-29 18:43:05 -0500

    Delete console.log line
    Delete console.log line

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const controller = require("../controllers/users");
 
-console.log(controller);
 router.route("/").get(controller.all);
 
 router.route("/signup").post(controller.createUser);
