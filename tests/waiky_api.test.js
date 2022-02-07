142722cd79f7af49e7c71140eb715293ba11f308
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-02-07 18:19:52 -0500

    Add sample test

--- tests/waiky_api.test.js
+++ tests/waiky_api.test.js
@@ -1,1 +1,21 @@
+const palindrome = (string) => {
+  return string.split("").reverse().join("");
+};
 
+test("palindrome of a", () => {
+  const result = palindrome("a");
+
+  expect(result).toBe("a");
+});
+
+test("palindrome of react", () => {
+  const result = palindrome("react");
+
+  expect(result).toBe("tcaer");
+});
+
+test("palindrome of releveler", () => {
+  const result = palindrome("releveler");
+
+  expect(result).toBe("releveler");
+});
